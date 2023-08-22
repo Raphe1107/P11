081c0b0c266fb5389eb5cc3fe3ea58427ff02cb4
Raphe1107 <rnuguet@gmail.com>
2023-08-22 18:15:12 +0200

    home update

--- home.php
+++ home.php
@@ -1,26 +1,60 @@
 <?php get_header() ?>
 
 <section class="hero">
-    <?php
-        $args = array(
-            'post_type' => 'photos', // Nom de votre custom post type
-            'posts_per_page' => -1, // Afficher tous les articles de ce type
-            'orderby' => 'rand',
-        );
-
-        $custom_query = new WP_Query($args);
-
-        while ( $custom_query->have_posts() ) : $custom_query->the_post(); the_post_thumbnail();
-        endwhile;
-
-        wp_reset_postdata();
-    ?>
     <h1 >PHOTOGRAPHE EVENT</h1>
 </section>
 
-<section class="post-filters">
-   
+<section class="filters">
+    <div class="cate_format">
+        <div class="category">
+            <form action="" method="get">
+                
+                <select name="category" id="category-select">
+                    <option value="all" hidden>CATÉGORIES</option>
+                    <option value="">Toutes les catégories</option>
+                    <?php
+                    $categories = get_terms(array(
+                        'taxonomy' => 'categorie', 
+                        'hide_empty' => false,
+                    ));
+
+                    foreach ($categories as $category) {
+                        echo '<option value="' . $category->slug . '">' . $category->name . '</option>';
+                    }
+                    ?>
+                </select>
+            </form>
+        </div>
+        <form action="" method="get">
+            <select name="format" id="format-select">
+                <option value="all" hidden>FORMATS</option>
+                <option value="">Tous les formats</option>
+                <?php
+                $formats = get_terms(array(
+                    'taxonomy' => 'format', 
+                    'hide_empty' => false,
+                ));
+
+                foreach ($formats as $format) {
+                    echo '<option value="' . $format->slug . '">' . $format->name . '</option>';
+                }
+                ?>
+            </select>
+        </form>
+    </div>
+
+    <form class="js-filter-form" method="get">	
+		<select id="date-select">
+            <option value="all" hidden>TRIER PAR</option>
+			<option value="DESC">Nouveautés</option>
+			<option value="ASC">Les plus anciennes</option>
+		</select>
+    </form>
+	
+    
 </section>
+
+
 
 
 
